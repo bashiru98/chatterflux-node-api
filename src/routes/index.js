962a568250f8426ad7245849cb3181f17c7254c7
bashiru98 <atulebashirubukari@gmail.com>
2023-09-10 02:15:39 +0300

    added new upate

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 // configure routes
 const express = require("express");
+const mongoose = require("mongoose");
 const models = require("../model");
 const dotenv = require("dotenv");
 const Openai = require("openai");
@@ -12,18 +13,20 @@ router.post("/stream", async (req, res) => {
   // get the email of the user making the request
   const user = req.query?.user;
   const prompt = req.query?.prompt;
-
+  const chatId = req?.query?.chatId;
   //  if there is no  user in the request body forbid the request
   if (!user) {
-    res.status(403).send("Forbidden");
+    res.status(403).json("Forbidden");
     return;
   }
 
   // if there is no prompt in the request body send bad request
   if (!prompt) {
-    res.status(400).send("Bad Request");
+    res.status(400).json("Bad Request");
     return;
   }
+
+  if (!chatId) return res.status(400).json("chatId is required");
 
   const stream = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
@@ -50,13 +53,14 @@ router.post("/stream", async (req, res) => {
     // date should be in milliseconds
     time: req?.body?.time || Date.now(),
     prompt,
+    chatId,
   });
 
   await message.save();
   // end request early and continue with other logics
   res.end(); // End the response stream when all parts are sent
   // check if there first message in the chat
-  const chat = await models.Chat.findOne({ user });
+  const chat = await models.Chat.findById(chatId);
 
   if (chat) {
     // first check if there first message in the chat
@@ -65,31 +69,35 @@ router.post("/stream", async (req, res) => {
       // if there is no first message in the chat update the message
       // first summarize the prompt using gpt and update the message
       // check if there first message in the chat
-      const chat = await models.Chat.findOne({ user });
+      // first check if there first message in the chat
+      const firstMessage = chat?.firstMessage;
+      if (!firstMessage) {
+        // if there is no first message in the chat update the message
+        // first summarize the prompt using gpt and update the message
+        const summarizePromptResponse = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content:
+                "summarize" +
+                " " +
+                messageAcc +
+                " " +
+                " into a nice short title that is relevant to the content" +
+                " " +
+                prompt,
+            },
+          ],
+          stream: false,
+        });
+        const firstMessage =
+          summarizePromptResponse?.choices[0]?.message?.content;
 
-      if (chat) {
-        // first check if there first message in the chat
-        const firstMessage = chat?.firstMessage;
-        if (!firstMessage) {
-          // if there is no first message in the chat update the message
-          // first summarize the prompt using gpt and update the message
-          const summarizePromptResponse = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [
-              {
-                role: "user",
-                content:
-                  "summarize" + " " + prompt + " " + " into a nice short title",
-              },
-            ],
-            stream: false,
-          });
-
-          const firstMessage =
-            summarizePromptResponse?.choices[0]?.message?.content;
-          chat.firstMessage = firstMessage;
-          await chat.save();
-        }
+        // update the chat with the first message
+        await models.Chat.findByIdAndUpdate(chatId, {
+          firstMessage,
+        });
       }
     }
   }
@@ -105,16 +113,16 @@ router.get("/chats", async (req, res) => {
     res.json(chats);
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).json("Internal Server Error");
   }
 });
 
 // get all messages for a specific chat
-router.get("/messages/:user", async (req, res) => {
+router.get("/messages", async (req, res) => {
   try {
-    const user = req.params?.user;
+    const chatId = req.query?.chatId;
 
-    const messages = await models.Message.find({ user });
+    const messages = await models.Message.find({ chatId });
 
     res.json(messages);
   } catch (error) {
@@ -127,10 +135,10 @@ router.get("/messages/:user", async (req, res) => {
 router.post("/chats", async (req, res) => {
   try {
     const user = req.body?.user;
-    const firstMessage = req.body?.firstMessage || "";
+    const firstMessage = req.body?.firstMessage;
 
-    if (!user || !firstMessage) {
-      res.status(400).send("Bad Request");
+    if (!user) {
+      res.status(400).json("Bad Request");
       return;
     }
 
@@ -142,7 +150,7 @@ router.post("/chats", async (req, res) => {
 
     await newChat.save();
 
-    res.json(chat);
+    res.json(newChat);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
@@ -154,12 +162,19 @@ router.delete("/chats/:id", async (req, res) => {
   try {
     const id = req.params?.id;
 
-    const deleteChat = await models.Chat.findByIdAndDelete(id);
+    const deleteChat = await models.Chat.findByIdAndDelete(
+      new mongoose.Types.ObjectId(id)
+    );
+
+    if (!deleteChat) return res.status(400).json("Failed to delete chat");
+
+    // delete all messages for this chat
+    await models.Message.deleteMany({ chatId: id });
 
     res.json(deleteChat);
   } catch (error) {
     console.log(error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).json("Internal Server Error");
   }
 });
 
